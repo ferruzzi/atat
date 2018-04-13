46ef8e8f451628812cd2c6881a50d36349d891a3
miststlkr <miststlkr@gmail.com>
2018-04-12 23:49:27 -0700

    Update atat.py

--- atat.py
+++ atat.py
@@ -4,67 +4,75 @@
 from pysnmp.hlapi import *
 
 ##########################
-#    VARIABLES
+#    SHAREABLE VARIABLES
 ##########################
 
-WHID = "location"
-CommunityString = "secret"
-Printers = ["printer1", "printer2","printer3", "plotter1", "printer4"]
+#WHID = "location"
+#CommunityString = "Secret"
+#Printers = ["prt-color-1", "prt-2", "3","mfp-2", "plotter-1"]
 
 ##########################
 #   SNMP Code Descriptions
 ##########################
 
-#   .1.3.6.1.2.1.43.11.1.1.5.1.1 = product [6="inkCartridge", 21="tonerCartridge", 15="fuser", 18="cleanerUnit", 20="transferUnit"]
-#productIndex = 5
-#productCodes = {"6" : "inkCartridge", "21" : "tonerCartridge", "15" : "fuser", "18" : "cleanerUnit", "20" : "transferUnit"}
-
+#   .1.3.6.1.2.1.43.11.1.1.5.1.1 = product 
+#     [6="inkCartridge", 21="tonerCartridge", 15="fuser", 18="cleanerUnit", 20="transferUnit"
+#
 #   .1.3.6.1.2.1.43.11.1.1.3.1.1 = color
-#      toner: [1="Black", 2="Cyan", 3="Magenta", 4="Yellow"]
-#      ink:   [1="Cyan", 2="Magenta", 3="Yellow", 4="Gray", 5="Matte Black", 6="Photo Black"]
-#colorIndex = 3
-#colorTonerCodes = {"1" : "black" , "2" : "cyan" , "3" : "magenta" , "4" : "yellow"}
-#colorInkCodes = {"1" : "cyan" , "2" : "magenta" , "3" : "yellow" , "4" : "gray" , "5" : "matte black", "6" : "photo black"}
-
-#   .1.3.6.1.2.1.43.11.1.1.7.1.1 = units [19="percent", 15="tenthsOfMilliliters"]
-#unitIndex = 7
-#unitCodes = {"19" : "percent" , "15" : "tenthsOfMilliliters"}
-
+#     toner: [1="Black", 2="Cyan", 3="Magenta", 4="Yellow"]
+#     ink:   [1="Cyan", 2="Magenta", 3="Yellow", 4="Gray", 5="Matte Black", 6="Photo Black"]
+#
+#   .1.3.6.1.2.1.43.11.1.1.7.1.1 = units 
+#     [19="percent", 15="tenthsOfMilliliters"]
+#
 #   .1.3.6.1.2.1.43.11.1.1.8.1.1 = max [int]
-#maxValueIndex = 8
-#maxValue = ""
+
+##########################
+#   Custom error messages
+##########################
+
+def HostNotFound(hostname):
+    print "Hostname %s not found. \n\t Printer may not exist or CommunityString is not correct." % hostname
+    return
+
+def Offline(hostname):
+    print "Unable to connect to printer %s.  Printer may be offline." % hostname
+    return
 
 
-class Printer:
+class Printer():
     short_name = ""
     hostname = ""
     is_mono = 0
-    style = "laser"
-    supplies_list = []
-
-def buildPrinter(short_name, hostname):
-    printer = Printer()
-    printer.short_name = short_name
-    printer.hostname = hostname
-    printer.style = getStyle(printer.hostname)
-    if printer.style =="none":
-        return printer;
-    printer.is_mono = getMono(printer.hostname, printer.style)
-    printer.colors = []
+    style = ""
+    colors = []
+    supplies = []
     
-    if printer.style == "laser":
-        printer.supplies_list = ["fuser", "cleaner", "transfer"]
-        if printer.is_mono:
-            printer.colors_list = ["black"]
-        else:
-            printer.colors_list = ["black", "cyan", "magenta", "yellow"]
-    elif printer.style == "plotter":
-        printer.colors_list = ["gray", "photo black", "matte black", "yellow", "magenta", "cyan"]
-
-    printer.colors = [[cl,""] for cl in printer.colors_list]
-    printer.supplies = [[su,""] for su in printer.supplies_list]
+    def __init__(self, short_name, hostname):
+        self.short_name = short_name
+        self.hostname = hostname
+        self.style = getStyle(hostname)
+        if self.style =="none":
+            return
+        self.is_mono = getMono(hostname, self.style)
         
-    return printer   
+        if self.style == "laser":
+            self.supplies_list = ["fuser", "cleaner", "transfer"]
+            if self.is_mono:
+                self.colors_list = ["black"]
+            else:
+                self.colors_list = ["black", "cyan", "magenta", "yellow"]
+        elif self.style == "plotter":
+            self.colors_list = ["gray", "photo black", "matte black", "yellow", "magenta", "cyan"]
+
+        self.colors = [[cl,""] for cl in self.colors_list]
+        # If it is a plotter, it won't have supplies.  Just carry on
+        try:
+            self.supplies = [[sl,""] for sl in self.supplies_list]
+        except:
+            return
+
+        return
 
 
 def getStyle(hostname):
@@ -81,8 +89,11 @@ def getStyle(hostname):
             return "laser"
         elif (varBinds[0][1] == 6):
             return "plotter"
+        else:
+            Offline(hostname)
+            return "none"
     except IndexError:
-        print "Hostname %s not found" % hostname
+        HostNotFound(hostname)
         return "none"
 
 
@@ -110,19 +121,18 @@ def loadColors(thisPrinter):
             ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.9.1.%s' % x)))
         )
     
-        try:
-            thisPrinter.colors[x-1][1] = int(varBinds[0][1])
-        except ValueError:
-            print "ValueError based on: %s" % str((varBinds[0][1]))
-        except IndexError:
-            print "Hostname %s not found" % thisPrinter.hostname
-            
+        thisPrinter.colors[x-1][1] = int(varBinds[0][1])
+        if thisPrinter.style == "plotter":
+            thisPrinter.colors[x-1][1] = int(thisPrinter.colors[x-1][1]/1.3)
     return
 
 
 def loadSupplies(thisPrinter):
+    if thisPrinter.style == "plotter":
+        # plotters don't have supplies lists
+        return
+    
     for x in range (len(thisPrinter.colors)+1, len(thisPrinter.colors)+len(thisPrinter.colors)+1):
-        
         errorIndication, errorStatus, errorIndex, supplyType = next(
             getCmd(SnmpEngine(),
             CommunityData(CommunityString, mpModel=0),
@@ -136,10 +146,12 @@ def loadSupplies(thisPrinter):
                 CommunityData(CommunityString, mpModel=0),
                 UdpTransportTarget((thisPrinter.hostname, 161)),
                 ContextData(),
-                ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.8.1.%s' % x)))
+                ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.9.1.%s' % x)))
             )
         supplyType = int(supplyType[0][1])
         supplyValue = int(supplyValue[0][1])
+# DEBUG:
+#        print "\tIndex: %s \n\tType: %s \n\tValue: %s \n" % (x, supplyType, supplyValue)
         
         # printer.supplies = ["fuser", "cleaner", "transfer"]
         # 15="fuser", 18="cleanerUnit", 20="transferUnit"
@@ -160,25 +172,38 @@ def loadSupplies(thisPrinter):
     return
 
 
-for i in Printers:
-    short_name = "%s-prt-%s" % (WHID.lower(), i.lower())
-    hostname = "%s.%s.work.com" % (short_name, WHID.lower())
+def printOutput(printer):
+    for x in range(0,len(printer.colors)):
+        print "\t", printer.colors[x]
+        
+    print "\t-------------------"
     
-    thisPrinter = buildPrinter(short_name, hostname)
-    if thisPrinter.style=="none":
-        continue
+    if printer.style == "plotter":
+        print "\tPlotters do not have supplies"
     else:
-        loadColors(thisPrinter)
-        if thisPrinter.style != "plotter":
-            loadSupplies(thisPrinter)
+        for x in range(0,len(printer.supplies)):
+                if not(isinstance(printer.supplies[x][1], basestring)):
+                    print "\t", printer.supplies[x]   
+                    
+
+
+if __name__ == "__main__":
     
-            
-    print "%s is a %s %s" % (thisPrinter.short_name, "mono" if thisPrinter.is_mono else "color", thisPrinter.style)
-    for x in range(0,len(thisPrinter.colors)):
-        if thisPrinter.style == "plotter":
-            thisPrinter.colors[x][1] = int(thisPrinter.colors[x][1]/1.3)
-        print "\t", thisPrinter.colors[x]
-    for x in range(0,len(thisPrinter.supplies)):
-        if thisPrinter.style != "plotter":
-            if not(isinstance(thisPrinter.supplies[x][1], basestring)):
-                print "\t", thisPrinter.supplies[x]
+    for i in Printers:
+        # If the user input the printer name starting with 'prt-' then remove the prefix to standardize
+        if i.startswith("prt-"):
+            i = i[4:]
+
+        short_name = "%s-prt-%s" % (WHID.lower(), i.lower())
+        hostname = "%s.%s.amazon.com" % (short_name, WHID.lower())
+
+        thisPrinter = Printer(short_name, hostname)
+        if thisPrinter.style != "none":
+            print "%s is a %s %s" % (thisPrinter.short_name, "mono" if thisPrinter.is_mono else "color", thisPrinter.style)
+        if thisPrinter.style=="none":
+            continue
+        else:
+            loadColors(thisPrinter)
+            loadSupplies(thisPrinter)
+
+        printOutput(thisPrinter)
