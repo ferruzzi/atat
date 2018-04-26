f166bccc8caecf5ef868d2b1a656f8e56cdcc3df
dgermynx <!Q@W3e4r>
2018-04-26 16:19:24 -0700

    - refactoring for python 3 string formatting - changing HostNotFound and Offline to Exceptions - Spaces instead of tabs for indentation - changing Exception logic in a try block - Added some print statements to debug when some code blocks are reached - refactored all print statements for python 3 (need brackets) - fixed some if/else logic

--- atat.py
+++ atat.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 # coding: utf-8
 
-from pysnmp.hlapi import *
 import sys
+
+from pysnmp.hlapi import *
+
 
 ##########################
 #    SHAREABLE VARIABLES
 ##########################
 
-#WHID = "location"
-#CommunityString = "Secret"
-#Printers = ["prt-color-1", "prt-2", "3","mfp-2", "plotter-1"]
+# WHID = "location"
+# CommunityString = "Secret"
+# Printers = ["prt-color-1", "prt-2", "3","mfp-2", "plotter-1"]
 
 
 ##########################
@@ -35,193 +37,203 @@ import sys
 ##########################
 
 class HostNotFoundException(Exception):
-	def __init__(self, hostname):
-		Exception.__init__(self, f"Hostname {hostname} not found.  \n\t  "
+    def __init__(self, hostname):
+        Exception.__init__(self, f"Hostname {hostname} not found.  \n\t  "
                                  f"Printer may not exist or CommunityString is not correct.")
+
 
 class OfflineException(Exception):
     def __init__(self, hostname):
         Exception.__init__(self, f"Unable to connect to printer {hostname}."
                                  f"Printer may be offline.")
 
-class Printer():
-	short_name = ""
-	hostname = ""
-	is_mono = 0
-	style = ""
-	colors = []
-	supplies = []
-    
-	def __init__(self, short_name, hostname):
-		self.short_name = short_name
-		self.hostname = hostname
-		self.style = getStyle(hostname)
-		if self.style is None:
-            print("Style is none, aborting Printer object initialization...")
-			return
-		self.is_mono = getMono(hostname, self.style)
-        
-		if self.style == "laser":
-			self.supplies_list = ["fuser", "cleaner", "transfer"]
-			if self.is_mono:
-				self.colors_list = ["black"]
-			else:
-				self.colors_list = ["black", "cyan", "magenta", "yellow"]
-		elif self.style == "plotter":
-			self.colors_list = ["gray", "photo black", "matte black", "yellow", "magenta", "cyan"]
 
-		self.colors = [[cl,""] for cl in self.colors_list]
-		# If it is a plotter, it won't have supplies.  Just carry on
-		try:
-			self.supplies = [[sl,""] for sl in self.supplies_list]
-		except Exception as e:
-            print(f"An unexpected error occured while creating supplies list: {e}")
+class Printer():
+    short_name = ""
+    hostname = ""
+    is_mono = 0
+    style = ""
+    colors = []
+    supplies = []
+
+    def __init__(self, short_name, hostname):
+        self.short_name = short_name
+        self.hostname = hostname
+        self.style = getStyle(hostname)
+        if self.style is None:
+
+    print("Style is none, aborting Printer object initialization...")
+    return
+
+
+self.is_mono = getMono(hostname, self.style)
+
+if self.style == "laser":
+    self.supplies_list = ["fuser", "cleaner", "transfer"]
+    if self.is_mono:
+        self.colors_list = ["black"]
+    else:
+        self.colors_list = ["black", "cyan", "magenta", "yellow"]
+elif self.style == "plotter":
+    self.colors_list = ["gray", "photo black", "matte black", "yellow", "magenta", "cyan"]
+
+self.colors = [[cl, ""] for cl in self.colors_list]
+# If it is a plotter, it won't have supplies.  Just carry on
+try:
+    self.supplies = [[sl, ""] for sl in self.supplies_list]
+except Exception as e:
+print(f"An unexpected error occured while creating supplies list: {e}")
+
 
 def loadVars(args):
-	global WHID
-	global CommunityString
-	global Printers
-	
-	if (len(args) < 2) or ("ipykernel_launcher.py" in args[0]):
-		print("No parameters passed, using defaults")
-	else:
-		try:
-			WHID = args[1]
-			CommunityString = args[2]
-			Printers = args[3].split(", ")
-		except IndexError:
-			print('Incorrect parameters. \n\tCorrect format is: atat.py [WHID] [CommunityString] ["printer1, printer2,... printer3"]')
-			sys.exit()
-	return
+    global WHID
+    global CommunityString
+    global Printers
+
+    if (len(args) < 2) or ("ipykernel_launcher.py" in args[0]):
+        print("No parameters passed, using defaults")
+    else:
+        try:
+            WHID = args[1]
+            CommunityString = args[2]
+            Printers = args[3].split(", ")
+        except IndexError:
+            print(
+                'Incorrect parameters. \n\tCorrect format is: atat.py [WHID] [CommunityString] ["printer1, printer2,... printer3"]')
+            sys.exit()
+    return
 
 
 def getStyle(hostname):
-	errorIndication, errorStatus, errorIndex, varBinds = next(
-		getCmd(SnmpEngine(),
-		CommunityData(CommunityString, mpModel=0),
-		UdpTransportTarget((hostname, 161)),
-		ContextData(),
-		ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.5.1.1')))
-	)
-    
-	try: 
-		if (varBinds[0][1] == 21):
-			return "laser"
-		elif (varBinds[0][1] == 6):
-			return "plotter"
-		else:
-			raise OfflineException(hostname)
-	except IndexError:
-		raise HostNotFoundException(hostname)
+    errorIndication, errorStatus, errorIndex, varBinds = next(
+        getCmd(SnmpEngine(),
+               CommunityData(CommunityString, mpModel=0),
+               UdpTransportTarget((hostname, 161)),
+               ContextData(),
+               ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.5.1.1')))
+    )
+
+    try:
+        if (varBinds[0][1] == 21):
+            return "laser"
+        elif (varBinds[0][1] == 6):
+            return "plotter"
+        else:
+            raise OfflineException(hostname)
+    except IndexError:
+        raise HostNotFoundException(hostname)
 
 
-def getMono(hostname, style):    
-	if style == "laser":
-		errorIndication, errorStatus, errorIndex, varBinds = next(
-			getCmd(SnmpEngine(),
-			CommunityData(CommunityString, mpModel=0),
-			UdpTransportTarget((hostname, 161)),
-			ContextData(),
-			ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.5.1.2')))
-		)
-		if (varBinds[0][1] == 21):
-			return False
-		return True
+def getMono(hostname, style):
+    if style == "laser":
+        errorIndication, errorStatus, errorIndex, varBinds = next(
+            getCmd(SnmpEngine(),
+                   CommunityData(CommunityString, mpModel=0),
+                   UdpTransportTarget((hostname, 161)),
+                   ContextData(),
+                   ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.5.1.2')))
+        )
+        if (varBinds[0][1] == 21):
+            return False
+        return True
+
 
 def loadColors(thisPrinter):
-	for x in range (1, len(thisPrinter.colors)+1):
-		errorIndication, errorStatus, errorIndex, varBinds = next(
-			getCmd(SnmpEngine(),
-			CommunityData(CommunityString, mpModel=0),
-			UdpTransportTarget((thisPrinter.hostname, 161)),
-			ContextData(),
-			ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.9.1.%s' % x)))
-		)
-		
-		thisPrinter.colors[x-1][1] = int(varBinds[0][1])
-		if thisPrinter.style == "plotter":
-			thisPrinter.colors[x-1][1] = int(thisPrinter.colors[x-1][1]/1.3)
-	return
+    for x in range(1, len(thisPrinter.colors) + 1):
+        errorIndication, errorStatus, errorIndex, varBinds = next(
+            getCmd(SnmpEngine(),
+                   CommunityData(CommunityString, mpModel=0),
+                   UdpTransportTarget((thisPrinter.hostname, 161)),
+                   ContextData(),
+                   ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.9.1.%s' % x)))
+        )
+
+        thisPrinter.colors[x - 1][1] = int(varBinds[0][1])
+        if thisPrinter.style == "plotter":
+            thisPrinter.colors[x - 1][1] = int(thisPrinter.colors[x - 1][1] / 1.3)
+    return
+
 
 def loadSupplies(thisPrinter):
-	if thisPrinter.style == "plotter":
-		# plotters don't have supplies lists
-		return
-    
-	for x in range (len(thisPrinter.colors)+1, len(thisPrinter.colors)+len(thisPrinter.colors)+1):
-		errorIndication, errorStatus, errorIndex, supplyType = next(
-			getCmd(SnmpEngine(),
-			CommunityData(CommunityString, mpModel=0),
-			UdpTransportTarget((thisPrinter.hostname, 161)),
-			ContextData(),
-			ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.5.1.%s' % x)))
-		)
-        
-		errorIndication, errorStatus, errorIndex, supplyValue = next(
-			getCmd(SnmpEngine(),
-			CommunityData(CommunityString, mpModel=0),
-			UdpTransportTarget((thisPrinter.hostname, 161)),
-			ContextData(),
-			ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.9.1.%s' % x)))
-		)
-		supplyType = int(supplyType[0][1])
-		supplyValue = int(supplyValue[0][1])
-# DEBUG:
-#        print "\tIndex: %s \n\tType: %s \n\tValue: %s \n" % (x, supplyType, supplyValue)
-        
-		# printer.supplies = ["fuser", "cleaner", "transfer"]
-		# 15="fuser", 18="cleanerUnit", 20="transferUnit"
-		if (supplyType == 1):
-			return
-		if (supplyType == 20):
-			thisPrinter.supplies[2][1] = supplyValue
-			continue
-		if (supplyType == 18):
-			thisPrinter.supplies[1][1] = supplyValue
-			continue
-		if (supplyType == 15):              
-			thisPrinter.supplies[0][1] = supplyValue
-			continue
-		else:
-			print(supplyType)
-	
-	return
+    if thisPrinter.style == "plotter":
+        # plotters don't have supplies lists
+        return
+
+    for x in range(len(thisPrinter.colors) + 1, len(thisPrinter.colors) + len(thisPrinter.colors) + 1):
+        errorIndication, errorStatus, errorIndex, supplyType = next(
+            getCmd(SnmpEngine(),
+                   CommunityData(CommunityString, mpModel=0),
+                   UdpTransportTarget((thisPrinter.hostname, 161)),
+                   ContextData(),
+                   ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.5.1.%s' % x)))
+        )
+
+        errorIndication, errorStatus, errorIndex, supplyValue = next(
+            getCmd(SnmpEngine(),
+                   CommunityData(CommunityString, mpModel=0),
+                   UdpTransportTarget((thisPrinter.hostname, 161)),
+                   ContextData(),
+                   ObjectType(ObjectIdentity('.1.3.6.1.2.1.43.11.1.1.9.1.%s' % x)))
+        )
+        supplyType = int(supplyType[0][1])
+        supplyValue = int(supplyValue[0][1])
+        # DEBUG:
+        #        print "\tIndex: %s \n\tType: %s \n\tValue: %s \n" % (x, supplyType, supplyValue)
+
+        # printer.supplies = ["fuser", "cleaner", "transfer"]
+        # 15="fuser", 18="cleanerUnit", 20="transferUnit"
+        if (supplyType == 1):
+            return
+        if (supplyType == 20):
+            thisPrinter.supplies[2][1] = supplyValue
+            continue
+        if (supplyType == 18):
+            thisPrinter.supplies[1][1] = supplyValue
+            continue
+        if (supplyType == 15):
+            thisPrinter.supplies[0][1] = supplyValue
+            continue
+        else:
+            print(supplyType)
+
+    return
+
 
 def printOutput(printer):
-	for x in range(0,len(printer.colors)):
-		print("\t", printer.colors[x])
-        
-	print("\t-------------------")
-    
-	if printer.style == "plotter":
-		print("\tPlotters do not have supplies")
-	else:
-		for x in range(0,len(printer.supplies)):
-			if not(isinstance(printer.supplies[x][1], basestring)):
-				print("\t", printer.supplies[x])
-                
+    for x in range(0, len(printer.colors)):
+        print("\t", printer.colors[x])
+
+    print("\t-------------------")
+
+    if printer.style == "plotter":
+        print("\tPlotters do not have supplies")
+    else:
+        for x in range(0, len(printer.supplies)):
+            if not (isinstance(printer.supplies[x][1], basestring)):
+                print("\t", printer.supplies[x])
+
 
 if __name__ == "__main__":
 
-	loadVars(sys.argv)
-	
-	for i in Printers:
-		# If the user input the printer name starting with 'prt-' then remove the prefix to standardize
-		if i.startswith("prt-"):
-			i = i[4:]
+    loadVars(sys.argv)
 
-		short_name = "%s-prt-%s" % (WHID.lower(), i.lower())
-		hostname = "%s.%s.amazon.com" % (short_name, WHID.lower())
+    for i in Printers:
+        # If the user input the printer name starting with 'prt-' then remove the prefix to standardize
+        if i.startswith("prt-"):
+            i = i[4:]
 
-		thisPrinter = Printer(short_name, hostname)
-		if thisPrinter.style is not None:
-			print("{shortname} is a {type} {style}".format(shortname=thisPrinter.short_name,
+        short_name = "%s-prt-%s" % (WHID.lower(), i.lower())
+        hostname = "%s.%s.amazon.com" % (short_name, WHID.lower())
+
+        thisPrinter = Printer(short_name, hostname)
+        if thisPrinter.style is not None:
+            print("{shortname} is a {type} {style}".format(shortname=thisPrinter.short_name,
                                                            type="mono" if thisPrinter.is_mono else "color",
                                                            style=thisPrinter.style))
-		elif thisPrinter.style is None:
-			continue
-		else:
-			loadColors(thisPrinter)
-			loadSupplies(thisPrinter)
+        elif thisPrinter.style is None:
+            continue
+        else:
+            loadColors(thisPrinter)
+            loadSupplies(thisPrinter)
 
-		printOutput(thisPrinter)
+        printOutput(thisPrinter)
